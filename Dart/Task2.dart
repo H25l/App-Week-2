6c35774de238cd95f0395a86c87aaafe2da24f5e
𝙃𝙖𝙯 <haz@Haz-5.local>
2023-09-30 17:30:40 +0300

    first push with simple list

---
+++ Dart/Task2.dart
@@ -0,0 +1,13 @@
+
+void main() {
+  int age = 30;
+  String name = 'John';
+  bool isStudent = true;
+  List<String> fruits = ['apple', 'banana', 'cherry'];
+
+  // Print variables
+  print('Age: $age');
+  print('Name: $name');
+  print('Is Student: $isStudent');
+  print('Fruits: $fruits');
+}
